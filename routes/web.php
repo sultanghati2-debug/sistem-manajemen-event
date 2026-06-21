8f40b0de4ef49fe0ffaa13f038212bb97d5cf933
abiifalik <mochfalikhabianawa1154@gmail.com>
2026-06-21 21:16:49 +0700

    Mengubah tampilan dashboard khusus admin.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// 3. Jalur Manajemen Profil (Harus Login)
+// 3. Jalur yang Harus Login (Terproteksi Middleware Auth)
 Route::middleware('auth')->group(function () {
+    
+    // Rute Baru: Admin Dashboard Layout
+    Route::get('/admin/dashboard', function () {
+        return view('admin.index');
+    })->name('admin.dashboard');
+
+    // Rute Manajemen Profil User
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
