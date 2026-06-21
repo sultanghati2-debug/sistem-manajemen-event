8faffe2af172cfcaf41030095f116fbfbef905f5
radipta pratama <2019iqbalpratama@gmail.com>
2026-06-21 22:49:41 +0700

    Mengatur logika seeder untuk meng-generate minimal satu akun dengan role Admin dan satu akun dengan role User secara otomatis untuk mempermudah pengujian.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Akun Admin
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        // Akun User biasa
+        User::updateOrCreate(
+            ['email' => 'user@example.com'],
+            [
+                'name' => 'User',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
