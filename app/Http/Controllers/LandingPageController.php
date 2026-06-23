9089725e1866c098dc4ac062d7c6b5960c2b4a31
Andrew-1312 <syarifwakwaww@gmail.com>
2026-06-23 13:28:30 +0700

    feat: prepare LandingPageController to receive search parameter

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -7,15 +7,18 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    public function index()
+        public function index(Request $request)
     {
-    // Ambil semua event
-    $events = \App\Models\Event::all();
-    
-    // Ambil 1 event pertama untuk Hero Section (atau bisa pakai logika lain)
-    $heroEvent = \App\Models\Event::latest()->first();
-    
-    // Kirim keduanya ke view
-    return view('welcome', compact('events', 'heroEvent'));
+        // 1. Tangkap request pencarian
+        $search = $request->input('search');
+
+        // Ambil semua event
+        $events = \App\Models\Event::all();
+        
+        // Ambil 1 event pertama untuk Hero Section (atau bisa pakai logika lain)
+        $heroEvent = \App\Models\Event::latest()->first();
+        
+        // 2. Kirim $events, $heroEvent, DAN $search ke view
+        return view('welcome', compact('events', 'heroEvent', 'search'));
     }
 }
